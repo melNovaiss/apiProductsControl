11b764752942f7eecb6618c8ad287d1a13dd296b
melNovaiss <mellnovais14@gmail.com>
2023-11-06 01:37:42 -0300

    config cors

--- src/main/java/br/com/apiProductsControl/config/WebConfig.java
+++ src/main/java/br/com/apiProductsControl/config/WebConfig.java
@@ -1,12 +1,6 @@
 package br.com.apiProductsControl.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,14 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8081")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Authorization")
-                .allowCredentials(true);
+                .allowedHeaders("*")
+                .exposedHeaders("Content-Type", "Authorization") // Adicione os cabeçalhos que deseja expor
+                .allowCredentials(true); // Se você estiver usando credenciais (como cookies)
     }
 }
+
 
 
 
